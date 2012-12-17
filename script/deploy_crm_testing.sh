add38f0ca0d4200c8825909464bfe7d030bf54b0
sofika <fischer@sofika.de>
2012-12-17 14:20:21 +0100

    test1

--- script/deploy_crm_testing.sh
+++ script/deploy_crm_testing.sh
@@ -5,13 +5,13 @@
 DIR=/tmp/test1
 rm -rf $DIR
 mkdir $DIR
-if test $? -e 0
+if test $? -eq  0
 then
 	echo "error: cannto create directory $DIR" >&2
 	exit 1
 fi
 cp -qr . $DIR
-if test $? -e 0
+if test $? -eq  0
 then
 	echo "error: cannot copy current directory to $DIR" >&2
 	exit 1
